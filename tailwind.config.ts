ff32e4252e00110237d917560eccb8e2d027fdc5
vamuq999 <vamuq999@gmail.com>
2026-03-01 14:44:42 +1100

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+// tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
